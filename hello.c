dfedbb4ebf22c5b4b97d5b438b3397c6f218a35a
bschaffart963 <42656545+bschaffart963@users.noreply.github.com>
2018-08-23 15:48:49 -0500

    Changes Made

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Matthew Schaffart Computer Science\n");
+  printf("Matthew Schaffart Computer Science, Brandon Schaffart - Computer Science\n");
 
   return 0;
 }
